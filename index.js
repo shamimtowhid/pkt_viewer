ec00bfe96c29a3d075ac95e5387f38bb19d06e04
shamimtowhid <shamim.towhid@gmail.com>
2023-02-11 16:02:10 -0600

    implementation of moving sin wave

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ const svg = d3.select("body")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
+
+
+let t = 0;
+
+setInterval(() => {
+
+    const data = d3.range(15).map(d => ({
+        x: d * 60 + 50,
+        y: 250 + Math.sin(d * 0.5 + t) * 220
+    }));
+
+    const circles = svg.selectAll("circle").data(data);
+    circles.enter().append("circle").attr("r", 20);
+    circles
+        .attr("cx", (d) => d.x)
+        .attr("cy", (d) => d.y);
+
+    t = t + 0.1;
+
+}, 50);
