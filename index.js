2543a647eb917c6c244b47df1aa4d722518e56ad
shamimtowhid <shamim.towhid@gmail.com>
2023-02-11 12:52:39 -0600

    added svg to the html

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 //import {select} from 'd3'; //This is ES6 syntax, not supported by all browser
 
-d3.select("#text").style("color", "green");
+//d3.select("#text").style("color", "green");
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const svg = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
