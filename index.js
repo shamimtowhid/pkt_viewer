0fc21d5363622d32174df12c209407a96639e461
shamimtowhid <shamim.towhid@gmail.com>
2023-02-12 00:23:35 -0600

    Complete scatter plot (basic)

--- index.js
+++ index.js
@@ -1,66 +1,25 @@
-//import {select} from 'd3'; //This is ES6 syntax, not supported by all browser
-
-//d3.select("#text").style("color", "green");
-
 const width = window.innerWidth;
 const height = window.innerHeight;
-
-const svg = d3.select("body")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-//const { json } = d3;
 
 const jsonUrl = ["https://gist.githubusercontent.com/", "shamimtowhid/",
     "58112041b5fd9787fe9c71cebebd08a2/",
     "raw/4673fda42780ae0a6c0319a068544a51db65260a/",
     "network_data.json"].join("");
 
+// converting string data to float
 const parseObject = (d) => {
     d.rcv_time = +d.rcv_time;
     d.send_time = +d.send_time;
     return d;
 };
 
-
-const main = async () => {
-    const data = await d3.json(jsonUrl)
-        .catch(error => {
-            console.error(error);
-        });
-    let parsedData = data.map(parseObject);
-
-    // return min and max for domain
-    const x = d3.scaleLinear()
-        .domain(d3.extent(parsedData, xValue))
-        .range([0, width]);
-
-    const y = d3.scaleLinear()
-        .domain(d3.extent(parsedData, yValue))
-        .range([height, 0]); // this range is flipped because origin is at upper left corner
-
-    // acts as a setter if we provide a value
-    // if we do not provide a value like below, it acts as a getter
-    //console.log(x.domain());
-
-    //console.log(x.range());
-
-    // svg.selectAll("circle")
-    //    .data(parsedData)
-    //    .join("circle")
-    //    .attr("cx",)
-    //    .attr("cy",)
-    //    .attr("r",)
-};
-
-main();
-
+// preparing X data
 const xValue = (d) => {
     const ts1 = 1675921681.915128;
     return d.send_time - ts1; // return difference between two timestamp in seconds
 };
 
+// preparing Y data
 const yValue = (d) => {
     let depth = 0;
     for (let i = 0; i < d.swtraces.length; i++) {
@@ -69,16 +28,67 @@ const yValue = (d) => {
     return depth
 };
 
+// data processing and plotting
+const main = async () => {
+    const data = await d3.json(jsonUrl)
+        .catch(error => {
+            console.error(error);
+        });
+    let parsedData = data.map(parseObject);
 
-// //console.log(jsonUrl);
-// d3.json(jsonUrl).then(data => {
-//     //console.log(data);
-//     let parsedData = data.map(parseObject);
-//     console.log(parsedData);
-// })
-//     .catch(error => {
-//         console.error(error);
-//     });
+    const margin = {
+        top: 40,
+        right: 40,
+        bottom: 40,
+        left: 40
+    };
+    const radius = 5;
+    // return min and max for domain
+    const x = d3.scaleLinear()
+        .domain(d3.extent(parsedData, xValue))
+        .range([margin.left, width - margin.right]);
+
+    // acts as a setter if we provide a value
+    // if we do not provide a value like below, it acts as a getter
+    //console.log(x.domain());
+    //console.log(x.range());
+
+    const y = d3.scaleLinear()
+        .domain(d3.extent(parsedData, yValue))
+        .range([height - margin.bottom, margin.top]); // this range is flipped because origin is at upper left corner
+
+
+    const marks = parsedData.map(d => ({
+        x: x(xValue(d)),
+        y: y(yValue(d)),
+    }));
+
+    const svg = d3.select("body")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    svg.selectAll("circle")
+        .data(marks)
+        .join("circle")
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+        .attr("r", radius);
+
+
+    svg.append("g")
+        .attr('transform', `translate(${margin.left}, 0)`)
+        .call(d3.axisLeft(y));
+
+    svg.append("g")
+        .attr('transform', `translate(0, ${height - margin.bottom})`)
+        .call(d3.axisBottom(x));
+};
+
+main();
+
+
+// code for moving circles, (sin wave)
 
 //let t = 0;
 
